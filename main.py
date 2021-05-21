fa3e71422c7d22657f184f4fbfb97d38b995c3c4
Aadithyaa <1aadithyaa@gmail.com>
2021-05-21 11:30:55 -0700

    FULLY FUNCTIONAL APP READY TO LAUNCH

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import pandas as pd
 import numpy as np
 
-data = np.load_csv('all.csv')
+df = pd.read_csv('all.csv')
+poems = df["content"]
+
+def random(poems):
+    return poems[0]
+
+print("Welcome to Langreader's Wonderful World of Language Learning\u2122")
+
+
+poem_url = input("gimme url for text:")
+print("here is a poem of similary difficulty:")
+print(random(poems))
+
+
+
